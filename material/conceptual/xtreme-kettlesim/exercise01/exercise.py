d388c07a1410eaf63703c18fd151e542faca8290
Oliver Maskery <omaskery@googlemail.com>
2014-03-01 12:43:05 +0000

    introduced exercise 01, decisions/branching

--- material/conceptual/xtreme-kettlesim/exercise01/exercise.py
+++ material/conceptual/xtreme-kettlesim/exercise01/exercise.py
@@ -2,9 +2,10 @@
 
 from kettlesim import *
 
-start_simulation("basics")
+start_simulation("decisions")
 
-fill_kettle()
+if kettle_is_empty():
+	fill_kettle()
 turn_on_kettle()
 wait_until_kettle_boiled()
 pour_water_into_cup()
--- material/conceptual/xtreme-kettlesim/kettlesim/__init__.py
+++ material/conceptual/xtreme-kettlesim/kettlesim/__init__.py
@@ -1,24 +1,65 @@
 
+import time
+import sys
+
 ROOM_TEMPERATURE = 26
 BOILING_TEMPERATURE = 100
 DANGER_TEMPERATURE = 45 # guesstimate at when something hurts
 DRINKABLE_TEMPERATURE = 60 # according to like the NHS, seems hot!
 
-def introduce_simulator():
-	print("WELCOME TO THE XTREME KETTLE SIMULATOR 3000")
-	print("THE FUTURE OF KETTLE SIMULATION IS HERE")
+class SimulationException(Exception):
+	pass
 
 class SimulationContext(object):
 	def __init__(self):
 		self.tick_count = 0
 		self.objects = []
 		self.running = False
+		self.challenges = None
+	def fetch(self, object_name):
+		for obj in self.objects:
+			if obj.name == object_name:
+				return obj
+		return None
+	def horizontal_line(self):
+		print("-" * 80)
+	def introduction(self):
+		self.horizontal_line()
+		print("WELCOME TO THE XTREME KETTLE SIMULATOR 3000")
+		print("THE FUTURE OF KETTLE SIMULATION IS HERE")
+		self.horizontal_line()
+	def goodbye(self):
+		self.horizontal_line()
+		print("XTREME KETTLE SIMULATOR 3000 EXITING")
+		print("RETURN SOON FOR MORE KETTLE-RELATED TEXTUAL ACTION")
+		self.horizontal_line()
+	def error(self, description):
+		self.horizontal_line()
+		print("XTREME KETTLE SIMULATOR ERROR:")
+		print(description)
+		self.horizontal_line()
+		sys.exit(-1)
+	def __del__(self):
+		self.goodbye()
+	def should_challenge(self, name):
+		if self.challenges is not None:
+			return (name in self.challenges.keys())
+		return False
 	def tick(self):
 		for obj in self.objects:
 			obj.tick(self)
 		self.tick_count += 1
+		time.sleep(0.01)
 	def add(self, obj):
 		self.objects.append(obj)
+	def say(self, message, with_time=True, with_newline=True):
+		display = message
+		if with_time:
+			display = "[tick %04i] %s" % (self.tick_count, message)
+		if with_newline:
+			print(display)
+		else:
+			print(display, end='')
 
 class SimulationObject(object):
 	def __init__(self, name):
@@ -26,7 +67,7 @@ class SimulationObject(object):
 	def tick(self, simulation):
 		pass
 
-class ContainerException(Exception):
+class ContainerException(SimulationException):
 	pass
 
 class ContainerOverfilledException(ContainerException):
@@ -39,7 +80,7 @@ class ContainerObject(object):
 	def accept(self, amount):
 		space = self.capacity - self.contains
 		accepted = amount
-		if space > amount:
+		if amount > space:
 			accepted = self.capacity - self.contains
 			self.contains = self.capacity
 			raise ContainerOverfilledException()
@@ -55,7 +96,7 @@ class ContainerObject(object):
 	def is_empty(self):
 		return (self.contains <= 0)
 
-class KettleException(Exception):
+class KettleException(SimulationException):
 	pass
 
 class Kettle(SimulationObject):
@@ -69,12 +110,12 @@ class Kettle(SimulationObject):
 		self.broken = False
 	def tick(self, simulation):
 		if self.broken:
-			raise KettleException("Stop trying to use a broken kettle!")
+			simulation_error("Stop trying to use a broken kettle!")
 			
 		if self.turned_on:
 			if self.water.is_empty():
 				self.broken = True
-				raise KettleException("BOOM! Kettle set to boil with no water! Your kettle broke.")
+				simulation_error("BOOM! Kettle set to boil with no water! Your kettle broke.")
 			self.temperature += self.boil_rate
 			if self.temperature > BOILING_TEMPERATURE:
 				self.turn_off()
@@ -92,24 +133,24 @@ class Kettle(SimulationObject):
 		try:
 			self.water.accept(water_amount)
 		except ContainerOverfilledException:
-			raise KettleException("Splash! You overfilled the kettle, water is everywhere!")
+			simulation_error("Splash! You overfilled the kettle, water is everywhere!")
 	def fill_completely(self):
 		amount = self.water.contains
 		try:
 			self.water.accept(self.water.capacity)
 		except ContainerOverfilledException:
 			if amount <= self.water.capacity:
-				raise KettleException("Splash! You overfilled the kettle, water is everywhere!")
+				simulation_error("Splash! You overfilled the kettle, water is everywhere!")
 			else:
-				raise KettleException("Splash! There's water everywhere, the kettle was already full!")
+				simulation_error("Splash! There's water everywhere, the kettle was already full!")
 	def pour_cup(self, cup):
 		if self.water.is_empty():
-			raise KettleException("Distressingly when you pour the kettle nothing comes out, there was no water in it!")
+			simulation_error("Distressingly when you pour the kettle nothing comes out, there was no water in it!")
 		
 		dispensed = self.water.dispense(1)
 		cup.fill(dispensed, self.temperature)
 
-class CupException(Exception):
+class CupException(SimulationException):
 	pass
 
 class Cup(SimulationObject):
@@ -127,11 +168,9 @@ class Cup(SimulationObject):
 			self.temperature = water_temperature
 		except ContainerOverfilledException:
 			if water_temperature >= DANGER_TEMPERATURE:
-				raise CupException("Argh! Really hot water overfills the cup!")
+				simulation_error("Argh! Really hot water overfills the cup!")
 			else:
-				raise CupException("Argh! Water overfilfs the cup! Luckily it's not too hot.")
-				
-introduce_simulator()
+				simulation_error("Argh! Water overfilfs the cup! Luckily it's not too hot.")
 
 global_default_kettle = Kettle()
 global_default_cup = Cup()
@@ -140,14 +179,56 @@ global_simulation_context = SimulationContext()
 global_simulation_context.add(global_default_kettle)
 global_simulation_context.add(global_default_cup)
 
+challenges = {
+	'basics': ({}, None),
+	'decisions': ({}, lambda context: context.fetch("Kettle").fill_completely()),
+	'repetitions': ({'boredom': True}, None)
+}
+
+def simulation_error(description):
+	global global_simulation_context
+	
+	global_simulation_context.error(description)
+
+def start_simulation(simulation_title):
+	global global_simulation_context
+	
+	if simulation_title in challenges.keys():
+		challenge_settings, configurator = challenges[simulation_title]
+		global_simulation_context.challenges = challenge_settings
+		
+		global_simulation_context.introduction()
+		
+		if configurator is not None:
+			configurator(global_simulation_context)
+	else:
+		simulation_error("Could not start xTreme Kettle Simulator, invalid simulation title '%s'!" % (simulation_title,))
+
+def kettle_is_empty():
+	global global_simulation_context
+	global global_default_kettle
+	
+	result = global_default_kettle.is_empty()
+	global_simulation_context.tick()
+	
+	state = "empty"
+	if not result:
+		state = "not empty"
+		
+	global_simulation_context.say("You check and find that the kettle is %s" % (state,))
+	
+	return result
+
 def fill_kettle():
 	global global_simulation_context
 	global global_default_kettle
 	
+	global_simulation_context.say("You start pouring water into the kettle...")
+	
 	global_default_kettle.fill_completely()
 	global_simulation_context.tick()
 
-	print("You fill the kettle with enough water for %s cups of tea!" % (global_default_kettle.water.contains,))
+	global_simulation_context.say("You fill the kettle with enough water for %s cups of tea!" % (global_default_kettle.water.contains,))
 
 def turn_on_kettle():
 	global global_simulation_context
@@ -157,18 +238,20 @@ def turn_on_kettle():
 		global_default_kettle.turn_on()
 		global_simulation_context.tick()
 		
-		print("You turn on the kettle and it starts to boil!")
+		global_simulation_context.say("You turn on the kettle and it starts to boil!")
 	else:
-		print("The kettle is already turned on, but you poke the button anyway. You feel reassured.")
+		global_simulation_context.say("The kettle is already turned on, but you poke the button anyway. You feel reassured.")
 
 def wait_until_kettle_boiled():
 	global global_simulation_context
 	global global_default_kettle
 	
+	global_simulation_context.say("You stare vacantly into space, waiting for the kettle.")
+	
 	while not global_default_kettle.is_boiled():
 		global_simulation_context.tick()
 	
-	print("The kettle has finished boiling!")
+	global_simulation_context.say("The kettle has finished boiling!")
 
 def pour_water_into_cup():
 	global global_simulation_context
@@ -177,4 +260,4 @@ def pour_water_into_cup():
 	
 	global_default_kettle.pour_cup(global_default_cup)
 	
-	print("You pour some water from the kettle into the cup.")
+	global_simulation_context.say("You pour some water from the kettle into the cup.")
